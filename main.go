eabb9ee97a25d019c217b7891db7d1659a43fe40
Frederic PERREAU <perreau.frederic@gmail.com>
2026-05-16 21:58:21 +0200

    Add main entry point for CLI application

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"IRETimer/cmd"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: task <command> [arguments]")
+		return
+	}
+
+	switch os.Args[1] {
+	case "list":
+		cmd.ListTasks()
+	case "add":
+		if len(os.Args) < 5 {
+			fmt.Println("Usage: task add <name> --cron <cron> --script <script>")
+			return
+		}
+		cmd.AddTask(os.Args[2], os.Args[4], os.Args[6])
+	case "edit":
+		cmd.EditTask(os.Args[2])
+	case "enable":
+		cmd.EnableTask(os.Args[2])
+	case "disable":
+		cmd.DisableTask(os.Args[2])
+	case "del":
+		cmd.DeleteTask(os.Args[2])
+	case "log":
+		cmd.ShowLogs()
+	default:
+		fmt.Println("Unknown command")
+	}
+}
